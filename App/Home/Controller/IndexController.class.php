0ff0bf54cb9e6f03e8c5a98cae70c3b43d107ed4
JohnsonZZ <465629989@qq.com>
2016-05-12 20:26:13 +0800

    v 1.2.0

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -3,7 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+		$list = M('News')->order('time desc')->limit(6)->select();
+		$this->assign('list',$list);
         $this->display();
 	}
-	
 }
